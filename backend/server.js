95dcc6191ce9eb8b200c2e3a2a5172f5ee98db30
avneeshkaur <avneeshkaur92@gmail.com>
2026-03-24 15:52:37 +0530

    fix cors for vercel

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,9 @@ const mongoose = require('mongoose')
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(cors())
+app.use(cors({
+  origin: '*'
+}))
 app.use(express.json())
 
 const apiRoutes = require('./routes/api')
